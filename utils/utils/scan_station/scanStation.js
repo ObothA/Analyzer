1050ffbb331bea9b5de9c41e52640e804a656e6b
obotha <khyper234@gmail.com>
2019-05-18 12:43:04 +0300

    d

--- utils/utils/scan_station/scanStation.js
+++ utils/utils/scan_station/scanStation.js
@@ -3,7 +3,7 @@
 
 
 function scanStation(id, pool){
-    const QUERY = `select hoursSinceEpoch_of_db_insertion from GeneralTable where stationID=${id} ORDER BY id DESC limit 1`;
+    const QUERY = `select hoursSinceEpoch_of_db_insertion from GeneralTable where stationID=${id} ORDER BY GeneralTable.id  DESC limit 1`;
 
     pool.query(QUERY, (queryError, result, fields) => {
         if (queryError){
