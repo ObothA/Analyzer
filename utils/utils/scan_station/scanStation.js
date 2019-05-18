b7c9a68d897a33c19476268244f84627d21f1157
obotha <khyper234@gmail.com>
2019-05-18 12:11:04 +0300

    l

--- utils/utils/scan_station/scanStation.js
+++ utils/utils/scan_station/scanStation.js
@@ -6,9 +6,9 @@ function scanStation(id, pool){
     const QUERY = `select hoursSinceEpoch_of_db_insertion from GeneralTable where stationID=${id} ORDER BY id DESC limit 1`;
 
     pool.query(QUERY, (queryError, result, fields) => {
-        if(queryError){
+        if (queryError){
             console.log(queryError);
-        } else {
+        } else if (result){
             const db_epoch_time = result[0].hoursSinceEpoch_of_db_insertion;
             if(db_epoch_time){
                 console.log(db_epoch_time);
